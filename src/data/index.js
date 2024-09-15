161f41157c061b055341830519fef54b8daef493
Avinash-singh1 <avinash202002@gmail.com>
2024-09-15 18:41:28 +0530

    Added changes

--- src/data/index.js
+++ src/data/index.js
@@ -25,19 +25,19 @@ export const navLinks = [
 
 const experiences = [
   {
-    title: "YouTube Content Creator",
+    title: "Digital Content Creator",
     company_name: "Self Employed",
     date: "2016 - Present",
     details: [
-      "Built a subscriber base of over <span style='color: white;'>500,000 subscribers</span> by creating video content to help programmers.",
-      "Crafted visually appealling programming videos that have garnered over <span style='color: white;'>30,000,000 views</span>.",
+      "Built a subscriber base of over <span style='color: white;'>4,000 subscribers</span> by creating video content to help programmers.",
+      "Crafted visually appealling programming videos that have garnered over <span style='color: white;'>30,000 views</span>.",
       "Produced high-quality educational and entertaining videos for clients including <span style='color: white;'>Intel, JetBrains, and MicroCenter</span>.",
     ],
   },
   {
     title: "Software Developer",
-    company_name: "Indie",
-    date: "2019 - 2023",
+    company_name: "Entero Heathcare Solution",
+    date: "2023 - 2024",
     details: [
       "Developed and delivered custom interdisciplinary coding portfolio for clients including <span style='color: white;'>Nvidia, Hostinger, and Amazon</span>.",
       "<span style='color: white;'>Designed and developed innovative</span> AI applications and interactive websites.",
@@ -46,8 +46,8 @@ const experiences = [
   },
   {
     title: "Software Engineer",
-    company_name: "Prime 3",
-    date: "2018 - 2019",
+    company_name: "Infogenx",
+    date: "2022 - 2023",
     details: [
       "Built custom enterprise applications for a <span style='color: white;'>Fortune 500 company</span> as a full-stack software engineer.",
       "Developed and maintained <span style='color: white;'>scalable backend services</span>, ensuring high availability for critical business applications.",
